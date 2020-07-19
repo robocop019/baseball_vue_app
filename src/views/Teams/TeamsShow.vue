fbd01e18415b1c91ddfe07534f8e754902378bb5
Chris Bowen <chrisbowen@comcast.net>
2020-07-19 17:01:35 -0500

    added team standings and pitcher stats

--- src/views/Teams/TeamsShow.vue
+++ src/views/Teams/TeamsShow.vue
@@ -3,12 +3,12 @@
     <div class="container">
       <div class="row">
         <div class="col align-self-center">
-          <h1> {{team.name}} </h1>
+          <h1> {{currentTeam.name}} </h1>
         </div>
 
       </div> <!-- end .row -->
 
-      <h3> Record: {{team.wins}} - {{team.losses}} - {{team.ties}} </h3>
+      <h3> Record: {{currentTeam.wins}} - {{currentTeam.losses}} - {{currentTeam.ties}} </h3>
 
       <div class="row">
         <div class="col-6">
@@ -29,7 +29,6 @@
           <table class="table table-striped table-dark table-hover">
             <thead>
               <tr>
-                <th>Central</th>
                 <th>Team</th>
                 <th>W</th>
                 <th>L</th>
@@ -37,13 +36,23 @@
                 <th>GB</th>
               </tr>
             </thead>
+
+            <tbody>
+              <tr v-for='team in teams'>
+                <td> {{team.name}} </td>
+                <td> {{team.wins}} </td>
+                <td> {{team.losses}} </td>
+                <td> {{team.ties}} </td>
+                <td> GBS </td>
+              </tr>
+            </tbody>
           </table>
         </div>
       </div>
 
       <div class="row">
         
-        <h4>Roster and Stats</h4>
+        <h4>Roster and Hitting Stats</h4>
 
         <table class="table table-striped table-dark table-hover">
 
@@ -92,19 +101,57 @@
 
       </div> <!-- end .row -->
 
+       <div class="row">
+        
+        <h4>Pitching Stats</h4>
+
+        <table class="table table-striped table-dark table-hover">
+
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">APP</th>
+              <th scope="col">GS</th>
+              <th scope="col">W</th>
+              <th scope="col">L</th>
+              <th scope="col">S</th>
+              <th scope="col">IP</th>
+              <th scope="col">H</th>
+              <th scope="col">R</th>
+              <th scope="col">ER</th>
+              <th scope="col">K</th>
+              <th scope="col">BB</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for='pitcher in pitchers'>
+              <th scope="row"> {{pitcher.number}} </th>
+              <td> {{pitcher.name}} </td>
+              <td> {{pitcher.pitching_stats.appearances}} </td>
+              <td> {{pitcher.pitching_stats.starts}} </td>
+              <td> {{pitcher.pitching_stats.wins}} </td>
+              <td> {{pitcher.pitching_stats.losses}} </td>
+              <td> {{pitcher.pitching_stats.saves}} </td>
+              <td> {{pitcher.pitching_stats.innings}} </td>
+              <td> {{pitcher.pitching_stats.hits}} </td>
+              <td> {{pitcher.pitching_stats.runs}} </td>
+              <td> {{pitcher.pitching_stats.earned_runs}} </td>
+              <td> {{pitcher.pitching_stats.strikeouts}} </td>
+              <td> {{pitcher.pitching_stats.walks}} </td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div> <!-- end .row -->
+
     </div> <!-- end .container -->
   </div> <!-- end .teams-show -->
 </template>
 
 <style>
-  li {
-    list-style: none;
-  }
 
-  .card {
-    text-align: center;
-    margin: auto;
-  }
 </style>
 
 <script>
@@ -113,17 +160,31 @@
   export default {
     data: function() {
       return {
-        team: {},
-        players: []
+        currentTeam: {},
+        division: '',
+        teams: [],
+        players: [],
+        pitchers: []
       };
     },
     created: function() {
+
       axios.get('/api/teams/' + this.$route.params.id).then(response => {
-        this.team = response.data;
+        this.currentTeam = response.data;
+        this.division = response.data.division;
+
+        axios.get('/api/teams?division=' + this.division).then(response => {
+          this.teams = response.data
+        });
       });
+
 
       axios.get('/api/players?team_id=' + this.$route.params.id).then(response => {
         this.players = response.data;
+      });
+
+      axios.get('/api/players?team_id=' + this.$route.params.id + '&pitcher=true').then(response => {
+        this.pitchers = response.data;
       });
     },
     methods: {}
